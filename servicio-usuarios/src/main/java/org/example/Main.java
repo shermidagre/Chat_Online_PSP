d76396f35b9f7e52cbf0cfb9d8c78addf7ebc3ed
samue <samuelhermida55@gmail.com>
2026-01-27 16:22:19 +0100

    .esto se me habia olvidado subirlo

--- servicio-usuarios/src/main/java/org/example/Main.java
+++ servicio-usuarios/src/main/java/org/example/Main.java
@@ -8,8 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // Exclude ALL Spring Security auto-configuration
-@ComponentScan("org.example")
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
